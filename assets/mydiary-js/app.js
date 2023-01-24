77e0543a3bbe634dc8262e09f72c6e412e51fc9b
Shannon Goddard <Uthingkithunk@aol.com>
2023-01-24 08:28:12 -0800

    deleted unused pics and minify pages js

--- assets/mydiary-js/app.js
+++ assets/mydiary-js/app.js
@@ -1,12 +1,1 @@
-//////retrieve local storage data
-//  Retrieve Plant Logo
-var plantLogo = localStorage.getItem('plantLogo');
-
-if (plantLogo) {
-  $("#mylogo").attr("src" , plantLogo);
-  document.getElementById("MyGrowText").style.display = "block";
-  document.getElementById("MyGrowAdd").style.display = "none";
-  $("#taskButton").show();
-};
-
-
+var plantLogo=localStorage.getItem("plantLogo");plantLogo&&($("#mylogo").attr("src",plantLogo),document.getElementById("MyGrowText").style.display="block",document.getElementById("MyGrowAdd").style.display="none",$("#taskButton").show());
